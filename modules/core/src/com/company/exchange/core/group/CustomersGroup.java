36fb09dc66aea908d91c09f3c0a86988e7b7486d
Savinov <savinov_vu@mail.ru>
2022-12-26 16:18:29 +0800

    create access constraint for disks

--- modules/core/src/com/company/exchange/core/group/CustomersGroup.java
+++ modules/core/src/com/company/exchange/core/group/CustomersGroup.java
@@ -1,9 +1,28 @@
 package com.company.exchange.core.group;
 
 import com.company.exchange.constant.AppConstants;
+import com.company.exchange.entity.AppUser;
+import com.company.exchange.entity.Disk;
+import com.haulmont.cuba.core.global.UserSessionSource;
 import com.haulmont.cuba.security.app.group.AnnotatedAccessGroupDefinition;
 import com.haulmont.cuba.security.app.group.annotation.AccessGroup;
+import com.haulmont.cuba.security.app.group.annotation.Constraint;
+import com.haulmont.cuba.security.entity.EntityOp;
 
 @AccessGroup(name = AppConstants.CUSTOMER_GROUP, parent = RootGroup.class)
 public class CustomersGroup extends AnnotatedAccessGroupDefinition {
+    private final UserSessionSource session;
+
+    public CustomersGroup(UserSessionSource session) {
+        this.session = session;
+    }
+
+    @Constraint(operations = {EntityOp.CREATE, EntityOp.READ, EntityOp.UPDATE, EntityOp.DELETE})
+    public boolean diskConstraints(Disk disk) {
+        AppUser user = disk.getTakenItem().getUser();
+        boolean diskHasManager = user.getGroupNames().equals("ManagerGroup");
+        String currentUserLogin = session.getUserSession().getUser().getLogin();
+        boolean diskHasCurrentUser = user.getLogin().equals(currentUserLogin);
+        return diskHasManager || diskHasCurrentUser;
+    }
 }
