c2cd054756128b2a305fe6eb7ac0b090285c4920
Savinov <savinov_vu@mail.ru>
2023-01-17 13:47:55 +0800

    RunReportAction run report from browser

--- modules/web/src/com/company/exchange/web/screens/appuser/AppUserBrowse.java
+++ modules/web/src/com/company/exchange/web/screens/appuser/AppUserBrowse.java
@@ -1,11 +1,22 @@
 package com.company.exchange.web.screens.appuser;
 
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.exchange.entity.AppUser;
+import com.haulmont.reports.gui.actions.RunReportAction;
+
+import javax.inject.Inject;
 
 @UiController("exchange_AppUser.browse")
 @UiDescriptor("app-user-browse.xml")
 @LookupComponent("appUsersTable")
 @LoadDataBeforeShow
 public class AppUserBrowse extends StandardLookup<AppUser> {
+    @Inject
+    private Button usersReportBtn;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        usersReportBtn.setAction(new RunReportAction());
+    }
 }
